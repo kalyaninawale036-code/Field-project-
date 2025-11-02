bf0dd1eb27cf62e6086d6f3a36b24f957212a4a2
Kalyani Nawale <kalyaninawale036@gmail.com>
2025-11-02 19:58:24 +0530

    first commit

---
+++ a3.py
@@ -0,0 +1,14 @@
+# This is a comment - it's ignored by the interpreter
+
+# Print a greeting
+print("Hello, World!")
+
+# Define two variables
+num1 = 10
+num2 = 25
+
+# Calculate their sum
+sum_result = num1 + num2
+
+# Print the sum
+print("The sum of", num1, "and", num2, "is:", sum_result)
